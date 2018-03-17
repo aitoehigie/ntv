3cbaac1f36f83fbdbbfc92306377fd4b72cd3074
aitoehigie <aitoehigie@gmail.com>
2018-03-17 20:59:28 +0100

    commit

---
+++ ntv/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from .__version__ import __version__
+
+__title__ = "Nairaland Terminal Viewer"
+__author__ = "Ehigie (pystar) Aito"
+__license__ = "The MIT License (MIT)"
+__copyright__ = "(c) 2018 Ehigie (pystar) Aito"
+
+
